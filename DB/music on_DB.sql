f4957efe87af02e04a12fa5f69812be74cb1da40
Eom-taeseon <37288329+Eom-taeseon@users.noreply.github.com>
2018-06-02 05:32:53 +0900

    파일 안내

--- DB/music on_DB.sql
+++ DB/music on_DB.sql
@@ -10,17 +10,19 @@ music_team varchar(20) NOT NULL,
 music_year year NOT NULL
 );
 
--- INSERT NEW DATABASE CODE. ID는 자동으로 올라가므로 일일이 숫자 써 넣을 필요는 없습니다
-INSERT INTO music (id, music_name, music_genre, music_style, music_team, music_year) VALUES 
-(
-2,
-'첫눈', -- music name
-'댄스', -- music genre
-'달달한 노래', -- music style (잔잔한 노래 / 신나는 노래 / 열정적인 노래 / 슬픈 노래 / 힘이 나는 노래 / 암울한 노래)
-'걸그룹', -- music team (남자 솔로 / 여자 솔로 / 보이그룹 / 걸그룹 / 혼성그룹 / 밴드 / 인디)
-'2017' -- music year
-);
 
+
+-- INSERT NEW DATABASE CODE. ID는 자동으로 올라가므로 일일이 숫자 써 넣을 필요는 없습니다
+INSERT INTO music (music_name, music_genre, music_style, music_team, music_year) VALUES 
+(
+'아주 NICE', -- music name
+'댄스', -- music genre
+'신나는 노래', -- music style (잔잔한 노래 / 신나는 노래 / 열정적인 노래 / 슬픈 노래 / 힘이 나는 노래 / 암울한 노래)
+'남성', -- music gender (남성 / 여성 / 혼성)
+'보이그룹', -- music team (솔로 / 아이돌 / 그룹 / 밴드 / 인디)
+'2016' -- music year
+);
+ALTER TABLE music ADD music_year YEAR;
 -- 전체 DB표 확인
 SELECT * FROM music;
 
@@ -33,21 +35,23 @@ SELECT * FROM music WHERE music_genre = '댄스';
 -- CHECK MUSIC_STYLE. 장르 확인 // (잔잔한 노래 / 신나는 노래 / 열정적인 노래 / 슬픈 노래 / 힘이 나는 노래 / 암울한 노래)
 SELECT * FROM music WHERE music_style = '댄스';
 
--- CHECK MUSIC_TEAM. 팀 확인 // (남자 솔로 / 여자 솔로 / 보이그룹 / 걸그룹 / 혼성그룹 / 밴드 / 인디)
+-- CHECK MUSIC_TEAM. 팀 확인 // (솔로 / 아이돌 / 그룹 / 밴드 / 인디)
 SELECT * FROM music WHERE music_team = '밴드';
 
--- CHECK MUSIC_YEAR. 년도 확인 // (남자 솔로 / 여자 솔로 / 보이그룹 / 걸그룹 / 혼성그룹 / 밴드 / 인디)
+-- CHECK MUSIC_YEAR. 년도 확인 // 
 SELECT * FROM music WHERE music_year = '2017';
 
 -- UPDATE music information. 수정 명령문 // 아직 잘 안되요 ㅠㅠ
 UPDATE music SET 
-music_name = '사랑 빛', 
-music_genre = '발라드', 
-music_style = '달달한 노래', 
-music_team = '밴드', 
-music_year = '2010'
+-- music_name = '사랑 빛', 
+-- music_genre = '발라드', 
+-- music_style = '달달한 노래', 
+music_gender = '남성',
+music_team = '솔로',
+music_year = '2001'
 WHERE
-music_name = '12내가 니편이 되어줄게(Feat. 하은)';
+id = 4;
+-- music_name = '12내가 니편이 되어줄게(Feat. 하은)';
 
 -- DELETE music. 삭제 명령문
 DELETE FROM music WHERE id = 2;
