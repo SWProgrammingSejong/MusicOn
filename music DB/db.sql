e2909f244b60e7e4ba8dac61c88f4825bd6a0a22
bbwwpark <bbwwpark@naver.com>
2018-06-08 14:37:20 +0900

    스키마 조금 수정했습니다.

--- music DB/db.sql
+++ music DB/db.sql
@@ -1,3 +1,5 @@
+/*
+
 create table music(
 id int Not null Primary Key,
 music_name varchar(50),
@@ -7,4 +9,20 @@ gender varchar(20),
 beat varchar(20),
 team varchar(20),
 years int(10)
+);
+
+*/
+
+# 수정한 스키마 
+
+create table music(
+
+MUSIC_NUM int Not null Primary Key, # 음악 고유번호
+MUSIC_NAME varchar(50), # 노래제목 
+GENRE varchar(20),      # 장르 (발라드, 댄스, 랩/힙합, 인디음악, 록/메탈, 트로트, 포크/블루스)
+STYLE varchar(20),      # 스타일 
+SINGER_INFO varchar(20), #가수정보 (남성솔로, 여성솔로, 보이그룹, 걸그룹, 혼성그룹)
+R_YEAR varchar(20), #발매년도
+MV_LINK varchar(100) #유튜브 링크
+
 );
