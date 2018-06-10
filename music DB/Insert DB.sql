1faf0501e5f15bcc79e819ed2a9f4bdcb3c27109
Eom-taeseon <37288329+Eom-taeseon@users.noreply.github.com>
2018-06-10 17:45:02 +0900

    DB 다시만듬

--- music DB/Insert DB.sql
+++ music DB/Insert DB.sql
@@ -1,8 +1,17 @@
-INSERT INTO music (music_name, music_genre, music_style, music_team, music_year) VALUES 
+INSERT INTO music (music_name, music_lang, music_genre, music_style, music_gender, music_team, music_year) VALUES 
 (
-'오아시스(Feat. 이재훈)', -- music name
-'댄스', -- music genre
-'여름 노래', -- music style (잔잔한 노래 / 신나는 노래 / 열정적인 노래 / 슬픈 노래 / 힘이 나는 노래 / 암울한 노래 / 여름 노래 / 겨울 노래)
-'걸그룹', -- music team (남자 솔로 / 여자 솔로 / 보이그룹 / 걸그룹 / 혼성그룹 / 밴드 / 인디)
-'2007' -- music year
+'그래서 그대는',
+'한국',
+'발라드',
+'열정적',
+'남성',
+'솔로',
+'2000~2010'
 );
+-- 전체 DB표 확인
+SELECT * FROM music;
+
+-- 잔잔함 / 서정적 / 몽환적 / 감성적 / 슬픔 / 신남 / 열정적 / 기타
+-- 1900년도 이전 / 1900~1970년 / 1970~1980년 / 1980~1990년 / 1990~2000년 / 2000~2010년 / 2010~2018년 / 잘 모르겠습니다.
+-- 댄스 / 발라드 / R&B / 락 / 일렉트로닉 / 트로트 / 힙합/랩 / 잘 모르겠습니다
+-- (솔로 / 아이돌 / 그룹 / 밴드 / 인디)
